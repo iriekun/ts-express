ea02d6199694a72214dd5f00dc398f3cf943cb22
iriekun <chandara.lmc@gmail.com>
2019-10-23 14:53:47 +0300

    remove dist

--- dist/logger.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const logger = (req, res, next) => {
-    console.log("Logging...");
-    next();
-};
-exports.default = logger;
